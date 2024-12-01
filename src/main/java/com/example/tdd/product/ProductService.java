6e8090025b991a942f29d14d5747924c473b877b
Advanc2d <phm940916@gmail.com>
2024-12-01 13:42:35 +0900

    실전! 스프링부트 상품-주문 API 개발로 알아보는 TDD 섹션 4 스프링부트 테스트 전환까지

---
+++ src/main/java/com/example/tdd/product/ProductService.java
@@ -0,0 +1,47 @@
+package com.example.tdd.product;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+class ProductService {
+    private final ProductPort productPort;
+
+    ProductService(ProductPort productPort) {
+        this.productPort = productPort;
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity addProduct(@RequestBody final AddProductRequest request) {
+        final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+
+        productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<GetProductResponse> getProduct(@PathVariable Long productId) {
+        final Product product = productPort.getProduct(productId);
+
+        GetProductResponse response = new GetProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getDiscountPolicy()
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    public void updateProduct(Long productId, UpdateProductRequest request) {
+        final Product product = productPort.getProduct(productId);
+        product.update(request.name(), request.price(), request.discountPolicy());
+
+        productPort.save(product);
+    }
+}
