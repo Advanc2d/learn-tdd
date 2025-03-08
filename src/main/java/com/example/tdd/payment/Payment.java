1b8b70a22b9d1eb056baac8b37461765d92af005
Advanc2d <phm940916@gmail.com>
2025-03-08 16:19:11 +0900

    🙈 refactor : [tdd] #comment 섹션 6 주문 결제 API 개발 - JPA 적용 - 25.03.08

--- src/main/java/com/example/tdd/payment/Payment.java
+++ src/main/java/com/example/tdd/payment/Payment.java
@@ -1,12 +1,25 @@
 package com.example.tdd.payment;
 
 import com.example.tdd.order.Order;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Order order;
-    private final String cardNumber;
+
+    @OneToOne
+    private Order order;
+
+    private String cardNumber;
 
 
     public Payment(Order order, String cardNumber) {
@@ -16,19 +29,7 @@ class Payment {
         this.cardNumber = cardNumber;
     }
 
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
     public int getPrice() {
         return order.getTotalPrice();
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
     }
 }
--- src/main/java/com/example/tdd/payment/PaymentAdapter.java
+++ src/main/java/com/example/tdd/payment/PaymentAdapter.java
@@ -1,23 +1,25 @@
 package com.example.tdd.payment;
 
 import com.example.tdd.order.Order;
-import com.example.tdd.product.DiscountPolicy;
-import com.example.tdd.product.Product;
+import com.example.tdd.order.OrderRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 class PaymentAdapter implements PaymentPort {
     private final PaymentGateway paymentGateway;
     private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
 
-    PaymentAdapter(PaymentGateway paymentGateway, PaymentRepository paymentRepository) {
+    PaymentAdapter(PaymentGateway paymentGateway, PaymentRepository paymentRepository, OrderRepository orderRepository) {
         this.paymentGateway = paymentGateway;
         this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public Order getOrder(Long orderId) {
-        return new Order(new Product("상품1", 1000, DiscountPolicy.NONE), 2);
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException("주문이 존재하지 않습니다."));
     }
 
     @Override
